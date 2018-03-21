b30db2154b2229af1210948ba654f85d672483e3
Daniel Munoz <daniel.munozdev@gmail.com>
2018-03-21 12:55:05 -0700

    add event listener to inputs to change css variables

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll('input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  console.log(this.name);
+  document.documentElement.style.setProperty('--' + this.name + '', this.value + suffix)
+  console.log(suffix);
+}
+
+inputs.forEach(function(input) {
+  input.addEventListener('change', handleUpdate);
+  input.addEventListener('mousemove', handleUpdate);
+});
